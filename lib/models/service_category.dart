b58af0ed9f850c4c5a384ec27272db5d5d3f9cda
Daulet <damelimm@gmail.com>
2025-06-04 15:34:09 +0500

    updated README.md

--- lib/models/service_category.dart
+++
@@ -1,9 +0,0 @@
-import 'package:flutter/material.dart';
-
-class ServiceCategory {
-  final String name;
-  final IconData icon;
-  final String? parentCategory;
-
-  ServiceCategory(this.name, this.icon, [this.parentCategory]);
-}
